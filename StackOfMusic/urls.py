9bfb723416c5e84ac18aec82750b38213cc69379
JungChaeMoon <jungchaemoon@gmail.com>
2019-09-12 17:41:07 +0900

    Added Music detail view

--- StackOfMusic/urls.py
+++ StackOfMusic/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, MusicDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('accounts/', include('accounts.urls', namespace='accounts'))
+    path('<int:music_id>', MusicDetailView.as_view(), name='music_detail'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
