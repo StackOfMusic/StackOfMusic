5ac0698e75c1b3aea54765cd5fd68206aa863aa5
JungChaeMoon <jungchaemoon@gmail.com>
2019-11-09 21:36:46 +0900

    Update Submusic Create

--- createmusic/serializer.py
+++ createmusic/serializer.py
@@ -1,28 +1,45 @@
 from rest_framework import serializers
 from music.models import Music, SubMusic
+from instrument.models import Instrument
 from StackOfMusic.serializer import UserSerializer, GenreSerializer
 
 
+class InstrumentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Instrument
+        fields = (
+            'name'
+        )
+
+
 class SubMusicSerializer(serializers.ModelSerializer):
+    contributor = UserSerializer()
+    # instrument = InstrumentSerializer()
+
     class Meta:
         model = SubMusic
-        fields = '__all__'
+        fields = [
+            # 'instrument'
+            'contributor',
+            'music_file',
+            'create_date',
+        ]
 
 
 class WorkingMusicRetrieveSerializer(serializers.ModelSerializer):
     owner = UserSerializer()
     genre = GenreSerializer()
-    sub_music = SubMusicSerializer(many=True, read_only=True)
+    sub_musics = SubMusicSerializer(many=True, read_only=True)
 
     class Meta:
         model = Music
-        fields = (
+        fields = [
             'genre',
             'title',
             'seed_file',
             'album_jacket',
             'owner',
             'create_date',
-            'like',
-            'sub_music',
-        )
+            'sub_musics',
+        ]
